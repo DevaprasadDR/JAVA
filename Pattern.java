93e0592b1ae08bef3e56bd495bcaecfc0a33bb5c
Kaviya.V <Kaviya.V@qualitestgroup.com>
2022-12-13 13:16:47 +0530

    Commit

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        int i,j,row;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the row");
+        row=sc.nextInt();
+        for(i=0;i<row;i++){
+            for(j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
